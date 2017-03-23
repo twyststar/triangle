f65c91f6719a39eee4ce70357cf3631f6f06d8a9
Ben Petrosky and Starlynne Start <starlynne@>
2017-03-23 16:00:51 -0700

    create logic for identifying 3 types of triangle

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#formOne").submit(function(event){
+    event.preventDefault();
+
+    var sideOne = parseInt($("input#side1").val());
+    var sideTwo = parseInt($("input#side2").val());
+    var sideThree = parseInt($("input#side3").val());
+
+    if (sideOne === sideTwo && sideOne === sideThree){
+      $("#answer").text("an equalatiral");
+    } else if (sideOne === sideTwo || sideTwo === sideThree || sideOne === sideThree){
+      $("#answer").text("an Isosceles");
+    } else {
+      $("#answer").text("a Scalene");
+    }
+});
+});
