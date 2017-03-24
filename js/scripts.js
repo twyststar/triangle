990d1032dca61e7f6bd44a697e133866cb829f2f
Ben Petrosky and Starlynne Start <ben@>
2017-03-23 18:34:39 -0700

    change HTML to allow only numbers in form

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,21 @@
-$(document).ready(function(){
-  $("#formOne").submit(function(event){
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
     event.preventDefault();
 
     var sideOne = parseInt($("input#side1").val());
     var sideTwo = parseInt($("input#side2").val());
     var sideThree = parseInt($("input#side3").val());
 
-    if (sideOne + sideTwo <= sideThree || sideTwo + sideThree <= sideOne || sideOne + sideThree <= sideTwo){
+    if (sideOne + sideTwo <= sideThree || sideTwo + sideThree <= sideOne || sideOne + sideThree <= sideTwo) {
       $("#answer").text("not a triangle")
-    } else if (sideOne === sideTwo && sideOne === sideThree){
+    } else if (sideOne === sideTwo && sideOne === sideThree) {
       $("#answer").text("an equalatiral");
-    } else if (sideOne === sideTwo || sideTwo === sideThree || sideOne === sideThree){
+    } else if (sideOne === sideTwo || sideTwo === sideThree || sideOne === sideThree) {
       $("#answer").text("an Isosceles");
     } else {
-      $("#answer").text("a Scalene");
+      $("#answer").text("a Scalene")
     }
-});
+
+
+  });
 });
